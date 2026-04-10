9918ec19bb2089a909ea4bb95471416a8ab496be
ChrOertlin <c.oertlin@gmail.com>
2026-04-10 17:37:57 +0200

    chore: treesitter

--- nvim/plugins.lua
+++ nvim/plugins.lua
@@ -9,8 +9,8 @@ vim.pack.add {
     { src = "https://github.com/nvim-lua/plenary.nvim"},
     { src = "https://github.com/nvim-telescope/telescope-fzf-native.nvim"},
     { src = "https://github.com/nvim-telescope/telescope.nvim"},
-    -- { src = ""},
-    -- { src = ""},
+    { src = "https://github.com/nvim-treesitter/nvim-treesitter", version = 'main' },
+    { src = "https://github.com/nvim-treesitter/nvim-treesitter-context", },
     -- { src = ""},
     -- { src = ""},
     -- { src = ""},
@@ -181,4 +181,47 @@ require("telescope").setup(
 
   extensions_list = { "themes", "terms" },
   extensions = {},
+})
+
+-- Treesitter
+--
+local ts_parsers = {
+  "bash",
+  "dockerfile",
+  "git_config", "git_rebase", "gitattributes", "gitcommit", "gitignore",
+  "go", "gomod", "gosum",
+  "json", "toml", "yaml",
+  "lua",
+  "make",
+  "markdown",
+  "python",
+  "terraform",
+  "vim", "vimdoc",
+}
+
+local nts = require("nvim-treesitter")
+nts.install(ts_parsers)
+vim.api.nvim_create_autocmd('PackChanged', { 
+    callback = function()
+        nts.update()
+    end
+})
+
+require("treesitter-context").setup({
+  max_lines = 3,
+  multiline_threshold = 1,
+  separator = '-',
+  min_window_height = 20,
+  line_numbers = true,
+})
+
+vim.api.nvim_create_autocmd("FileType", { -- enable treesitter highlighting and indents
+  callback = function(args)
+    local filetype = args.match
+    local lang = vim.treesitter.language.get_lang(filetype)
+    if vim.treesitter.language.add(lang) then
+      vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+      vim.treesitter.start()
+    end
+  end
 })
