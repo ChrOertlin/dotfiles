5f916321e6d69ea89a5b36cfbd28e29409b9f806
ChrOertlin <c.oertlin@gmail.com>
2026-04-10 17:37:59 +0200

    chore: add lualine

--- nvim/plugins.lua
+++ nvim/plugins.lua
@@ -5,7 +5,7 @@ vim.pack.add{
     { src = "https://github.com/mason-org/mason-lspconfig.nvim"},
     { src = "https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim"},
     { src = "https://github.com/kdheepak/lazygit.nvim"},
-    -- { src = ""},
+    { src = "https://github.com/nvim-lualine/lualine.nvim"},
     -- { src = ""},
     -- { src = ""},
     -- { src = ""},
@@ -99,3 +99,60 @@ vim.lsp.config('gopls', {
 require('mason-lspconfig').setup({
   automatic_enable = true,
 })
+
+-- Lualine
+--
+require('lualine').setup {
+  options = {
+    icons_enabled = true,
+    theme = 'auto',
+    component_separators = { left = '', right = ''},
+    section_separators = { left = '', right = ''},
+    disabled_filetypes = {
+      statusline = {},
+      winbar = {},
+    },
+    ignore_focus = {},
+    always_divide_middle = true,
+    always_show_tabline = true,
+    globalstatus = false,
+    refresh = {
+      statusline = 1000,
+      tabline = 1000,
+      winbar = 1000,
+      refresh_time = 16, -- ~60fps
+      events = {
+        'WinEnter',
+        'BufEnter',
+        'BufWritePost',
+        'SessionLoadPost',
+        'FileChangedShellPost',
+        'VimResized',
+        'Filetype',
+        'CursorMoved',
+        'CursorMovedI',
+        'ModeChanged',
+      },
+    }
+  },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'branch', 'diff', 'diagnostics'},
+    lualine_c = {'filename'},
+    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_y = {'progress'},
+    lualine_z = {'location'}
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {'filename'},
+    lualine_x = {'location'},
+    lualine_y = {},
+    lualine_z = {}
+  },
+  tabline = {},
+  winbar = {},
+  inactive_winbar = {},
+  extensions = {}
+}
